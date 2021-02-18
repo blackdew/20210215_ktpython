804ed6fd6f87f633c5985db27b31534af71120c7
sookbun <blackdew7@gmail.com>
2021-02-18 17:11:38 +0900

    web2css

--- app.py
+++ app.py
@@ -4,12 +4,26 @@ app = Flask(__name__)
 @app.route('/')
 def index():
     with open("html/index.html", 'r', encoding='utf8') as f:
-        html = f.read()
-    return html
+        txt = f.read()
+    return txt
 
-@app.route('/hello')
-def hello_world():
-    return 'Hello World!'
+@app.route('/1.html')
+def html():
+    with open("html/1.html", 'r', encoding='utf8') as f:
+        txt = f.read()
+    return txt
+
+@app.route('/2.html')
+def css():
+    with open("html/2.html", 'r', encoding='utf8') as f:
+        txt = f.read()
+    return txt
+
+@app.route('/3.html')
+def js():
+    with open("html/3.html", 'r', encoding='utf8') as f:
+        txt = f.read()
+    return txt
 
 if __name__ == '__main__':
     app.run()
