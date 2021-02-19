40bccb66f04395d260e96418e2fae1a6873879ce
sookbun <blackdew7@gmail.com>
2021-02-19 13:36:42 +0900

    use template

--- app.py
+++ app.py
@@ -1,16 +1,13 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template
+app = Flask(__name__, template_folder='templates')
 app.config['ENV'] = 'development'
 app.config['DEBUG'] = True
 
 @app.route('/')
 def index():
-    with open("html/template.html", 'r', encoding='utf8') as f:
-        txt = f.read()
-
     title = "Welcome"
     content = "The World Wide Web (abbreviated WWW or the Web) is an information space where documents and other web resources are identified by Uniform Resource Locators (URLs), interlinked by hypertext links, and can be accessed via the Internet. English scientist Tim Berners-Lee invented the World Wide Web in 1989. He wrote the first web browser computer program in 1990 while employed at CERN in Switzerland. The Web browser was released outside of CERN in 1991, first to other research institutions starting in January 1991 and to the general public on the Internet in August 1991."
-    return txt.format(title=title, content=content)
+    return render_template('template.html', title=title, content=content)
 
 @app.route('/html')
 def html():
