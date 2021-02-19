44790a4debf825caf9f8a46718193b06d6abba3b
sookbun <blackdew7@gmail.com>
2021-02-19 13:38:46 +0900

    모든 페이지에 template 기능 적용

--- app.py
+++ app.py
@@ -11,30 +11,21 @@ def index():
 
 @app.route('/html')
 def html():
-    with open("html/template.html", 'r', encoding='utf8') as f:
-        txt = f.read()
-        
     title = "HTML"
     content = "Hypertext Markup Language (HTML) is the standard markup language for creating web pages and web applications. With Cascading Style Sheets (CSS) and JavaScript it forms a triad of cornerstone technologies for the World Wide Web.[2] Web browsers receive HTML documents from a web server or from local storage and render them into multimedia web pages. HTML describes the structure of a web page semantically and originally included cues for the appearance of the document."
-    return txt.format(title=title, content=content)
+    return render_template('template.html', title=title, content=content)
 
 @app.route('/css')
 def css():
-    with open("html/template.html", 'r', encoding='utf8') as f:
-        txt = f.read()
-        
     title = "CSS"
     content = "Cascading Style Sheets (CSS) is a style sheet language used for describing the presentation of a document written in a markup language.[1] Although most often used to set the visual style of web pages and user interfaces written in HTML and XHTML, the language can be applied to any XML document, including plain XML, SVG and XUL, and is applicable to rendering in speech, or on other media. Along with HTML and JavaScript, CSS is a cornerstone technology used by most websites to create visually engaging webpages, user interfaces for web applications, and user interfaces for many mobile applications."
-    return txt.format(title=title, content=content)
+    return render_template('template.html', title=title, content=content)
 
 @app.route('/javascript')
 def js():
-    with open("html/template.html", 'r', encoding='utf8') as f:
-        txt = f.read()
-        
     title = "Javascript"
     content = "JavaScript (/ˈdʒɑːvəˌskrɪpt/), often abbreviated as JS, is a high-level, dynamic, weakly typed, object-based, multi-paradigm, and interpreted programming language. Alongside HTML and CSS, JavaScript is one of the three core technologies of World Wide Web content production. It is used to make webpages interactive and provide online programs, including video games. The majority of websites employ it, and all modern web browsers support it without the need for plug-ins by means of a built-in JavaScript engine. Each of the many JavaScript engines represent a different implementation of JavaScript, all based on the ECMAScript specification, with some engines not supporting the spectrum fully, and with many engines supporting additional features beyond ECMA."
-    return txt.format(title=title, content=content)
+    return render_template('template.html', title=title, content=content)
 
 @app.route('/movies')
 def movie():
